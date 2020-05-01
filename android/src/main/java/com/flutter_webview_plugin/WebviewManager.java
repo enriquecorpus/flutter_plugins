ab4b21779dbd81646f3d952e4585174bf16a9a8b
Enrique Corpus <enriquecorpus@skydev.solutions>
2020-05-01 19:56:25 +0800

    test

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -283,17 +283,17 @@ class WebviewManager {
                 }
 
 
-            View decorView = activity.getWindow().getDecorView();
-            decorView.setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_IMMERSIVE
-            // Set the content to appear under the system bars so that the
-            // content doesn't resize when the system bars hide and show.
-            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            // Hide the nav bar and status bar
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+            // View decorView = activity.getWindow().getDecorView();
+            // decorView.setSystemUiVisibility(
+            // View.SYSTEM_UI_FLAG_IMMERSIVE
+            // // Set the content to appear under the system bars so that the
+            // // content doesn't resize when the system bars hide and show.
+            // | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            // | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            // | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            // // Hide the nav bar and status bar
+            // | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            // | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
 
                 // add custom view to container and save reference
@@ -326,11 +326,11 @@ class WebviewManager {
                 rootView.removeView(customView);
                 customViewCallback.onCustomViewHidden();
                 customView = null;
-                  View decorView = activity.getWindow().getDecorView();
-                decorView.setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            //       View decorView = activity.getWindow().getDecorView();
+            //     decorView.setSystemUiVisibility(
+            // View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            // | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            // | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
             }
 
