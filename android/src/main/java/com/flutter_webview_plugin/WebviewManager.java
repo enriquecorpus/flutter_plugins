88e074f4f23ca5ba2c5ce3c9b84221f49d545fc6
Enrique Corpus <enriquecorpus@skydev.solutions>
2020-05-01 20:08:10 +0800

    working fullscreen

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -283,19 +283,6 @@ class WebviewManager {
                 }
 
 
-            // View decorView = activity.getWindow().getDecorView();
-            // decorView.setSystemUiVisibility(
-            // View.SYSTEM_UI_FLAG_IMMERSIVE
-            // // Set the content to appear under the system bars so that the
-            // // content doesn't resize when the system bars hide and show.
-            // | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            // | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            // | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            // // Hide the nav bar and status bar
-            // | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            // | View.SYSTEM_UI_FLAG_FULLSCREEN);
-
-
                 // add custom view to container and save reference
                 customView = view;
                 customViewCallback = callback;
@@ -308,6 +295,22 @@ class WebviewManager {
                 FrameLayout contentView = (FrameLayout)rootView.findViewById(android.R.id.content);
                 contentView.setVisibility(View.GONE);
                 
+                try{
+                     View decorView = activity.getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            // Set the content to appear under the system bars so that the
+            // content doesn't resize when the system bars hide and show.
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            // Hide the nav bar and status bar
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                }
+                catch (Exception e) {
+                        
+                 }
             }
 
             @Override
@@ -326,11 +329,21 @@ class WebviewManager {
                 rootView.removeView(customView);
                 customViewCallback.onCustomViewHidden();
                 customView = null;
-            //       View decorView = activity.getWindow().getDecorView();
-            //     decorView.setSystemUiVisibility(
-            // View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            // | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            // | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+                try{
+                
+                    View decorView = activity.getWindow().getDecorView();
+                    decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+                } 
+                catch (Exception e) {
+                        
+                 }
+
+          
 
             }
 
