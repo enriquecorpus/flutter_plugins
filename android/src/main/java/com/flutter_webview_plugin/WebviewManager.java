604a857b6c47dc2be29516d3346181746a9d02cc
Enrique Corpus <enriquecorpus@skydev.solutions>
2020-08-14 18:22:25 +0800

    try

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -292,22 +292,22 @@ class WebviewManager {
                 FrameLayout contentView = (FrameLayout)rootView.findViewById(android.R.id.content);
                 contentView.setVisibility(View.GONE);
                 
-            //     try{
-            //          View decorView = activity.getWindow().getDecorView();
-            // decorView.setSystemUiVisibility(
-            // View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            // // Set the content to appear under the system bars so that the
-            // // content doesn't resize when the system bars hide and show.
-            // | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            // | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            // | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            // // Hide the nav bar and status bar
-            // | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            // | View.SYSTEM_UI_FLAG_FULLSCREEN);
-            //     }
-            //     catch (Exception e) {
+                try{
+                     View decorView = activity.getWindow().getDecorView();
+                    decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    // Hide the nav bar and status bar
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                }
+                catch (Exception e) {
                         
-            //      }
+                 }
             }
 
             @Override
